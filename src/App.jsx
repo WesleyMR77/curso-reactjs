cc36a0e9768ab03bae7fc92cbb5d7fc726412c4f
WesleyMR77 <wesleymonare@hotmail.com>
2022-02-02 17:23:27 -0400

    2.conceitos importantes finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import '../src/styles/global.scss'
+import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 
 export function App() {
-    return <RepositoryList/>
+    return (
+        <>
+            <RepositoryList/>
+            <Counter/>
+        </>
+    )
 }
